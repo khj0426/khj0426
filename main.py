cc0f1742a77e1c1213bccbc5f3873d4242d8bede
kimhyojung <706shin1728@naver.com>
2025-02-03 00:37:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ markdown_text = """
 - 늘 편안한 팀원이 되고자 하는 프론트엔드 개발자 김효중입니다. 
 - 기술을 선택할 때 “왜”라는 물음을 집요하게 파고들며 함께 성장하는 즐거움과,깊이 학습하는 것의 중요성을 알고 있습니다.
 
-## ✅ Latest Blog Post
+## ✅ 최근 블로그 글
 
 """  # list of blog posts will be appended here
 
